d74d96675ab0f9f18badbe2507e0ca8540496d85
liuyinglao <lyl940427@gmail.com>
2022-07-14 00:18:01 -0700

    test

--- react-native-pytorch-core/example/android/app/src/main/java/org/pytorch/rn/core/example/MainApplication.java
+++ react-native-pytorch-core/example/android/app/src/main/java/org/pytorch/rn/core/example/MainApplication.java
@@ -34,15 +34,11 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-import org.pytorch.rn.core.jsi.PyTorchCoreJSIModulePackage;
           // Packages that cannot be autolinked yet can be added manually here, for
           // PyTorchCoreExample:
           // packages.add(new MyReactNativePackage());
-import org.pytorch.rn.core.jsi.PyTorchCoreJSIModulePackage;
           packages.add(new PyTorchCorePackage());
-import org.pytorch.rn.core.jsi.PyTorchCoreJSIModulePackage;
           return packages;
-import org.pytorch.rn.core.jsi.PyTorchCoreJSIModulePackage;
         }
 
         @Override
@@ -65,7 +61,6 @@ import org.pytorch.rn.core.jsi.PyTorchCoreJSIModulePackage;
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-import org.pytorch.rn.core.jsi.PyTorchCoreJSIModulePackage;
 
     initializeFlipper(
         this,
